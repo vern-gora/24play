95b693849f32966b5e8c4fafe01e84b07a70d2a3
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-08-16 00:19:30 +0300

    Stickman on TrackFloor

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import * as THREE from "https://threejs.org/build/three.module.js";
-import { GLTFLoader } from "https://threejs.org/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "three";
+import { AnimationClip, VectorKeyframeTrack } from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const gameContainer = document.getElementById("game-container");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -9,148 +10,109 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer();
+
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
-gameContainer.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
+scene.background = new THREE.Color(0x89cdff);
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const track = new THREE.Mesh(planeGeometry, planeMaterial);
-track.position.set(0, -1, 0);
-scene.add(track);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const loader = new GLTFLoader();
+let loader = new GLTFLoader();
 
-loader.load("./threejs_tz/Brain.glb", (brainGltf) => {
-  const brainModel = brainGltf.scene;
-  const numberOfBrains = 10;
+let mixer;
+let stickmanModel;
 
-  for (let i = 0; i < numberOfBrains; i++) {
-    const brain = brainModel.clone();
-    const x = Math.random() * 10 - 5;
-    const z = Math.random() * 10 - 5;
-    brain.position.set(x, 0, z);
-    scene.add(brain);
+loader.load(
+  "./threejs_tz/Stickman.glb",
+  function (gltf) {
+    let stickmanModel = gltf.scene;
+    stickmanModel.traverse(function (node) {
+      if (node.isMesh) {
+        node.castShadow = true;
+        node.receiveShadow = true;
+      }
+    });
+
+    scene.add(stickmanModel);
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.code) {
+        case "ArrowUp":
+          stickmanModel.position.z += -3.5;
+          break;
+        case "ArrowLeft":
+          stickmanModel.position.x += -3.5;
+          break;
+        case "ArrowDown":
+          stickmanModel.position.z += 3.5;
+          break;
+        case "ArrowRight":
+          stickmanModel.position.x += 3.5;
+          break;
+      }
+    });
+
+    const rotationQuaternion = new THREE.Quaternion();
+    rotationQuaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
+
+    stickmanModel.setRotationFromQuaternion(rotationQuaternion);
+
+    mixer = new THREE.AnimationMixer(stickmanModel);
+    const clips = gltf.animations;
+    const clip = THREE.AnimationClip.findByName(clips, "Run");
+    const action = mixer.clipAction(clip);
+    action.play();
+  },
+  undefined,
+  function (error) {
+    console.error(error);
   }
-});
+);
 
-loader.load("./threejs_tz/Stickman.glb", (gltf) => {
-  const character = gltf.scene;
-  scene.add(character);
-
-  const mixer = new THREE.AnimationMixer(character);
-  const animations = gltf.animations;
-
-  if (animations && animations.length) {
-    const runAnimation = mixer.clipAction(animations[0]);
-    runAnimation.play();
+loader.load(
+  "./threejs_tz/TrackFloor.glb",
+  function (gltf) {
+    let trackFloor = gltf.scene;
+    trackFloor.traverse(function (node) {
+      if (node.isMesh) {
+        node.receiveShadow = true;
+      }
+    });
+    trackFloor.position.z = 2;
+    trackFloor.scale.z = 5;
+    scene.add(trackFloor);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
   }
+);
 
-  function updateAnimations(deltaTime) {
-    mixer.update(deltaTime);
-  }
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.x = -3;
+light.position.y = 3;
+light.position.z = 2;
+light.castShadow = true;
+scene.add(light);
 
-  function updateCharacterMovement() {
-    character.position.z -= 0.05;
-  }
-
-  function animate() {
-    const deltaTime = clock.getDelta();
-
-    updateCharacterMovement();
-    updateAnimations(deltaTime);
-
-    renderer.render(scene, camera);
-
-    requestAnimationFrame(animate);
-  }
-
-  animate();
-});
-
-loader.load("./threejs_tz/TrackFloor.glb", (gltf) => {
-  const trackModel = gltf.scene;
-  scene.add(trackModel);
-});
-
-const touchStartX = 0;
-const touchEndX = 0;
-
-gameContainer.addEventListener("touchstart", (event) => {
-  touchStartX = event.touches[0].clientX;
-});
-
-gameContainer.addEventListener("touchmove", (event) => {
-  touchEndX = event.touches[0].clientX;
-});
-
-gameContainer.addEventListener("touchend", () => {
-  const swipeDistance = touchEndX - touchStartX;
-
-  if (swipeDistance > 0) {
-    character.position.x += 0.5;
-  } else if (swipeDistance < 0) {
-    character.position.x -= 0.5;
-  }
-
-  touchStartX = 0;
-  touchEndX = 0;
-});
-
-const startScreen = document.getElementById("start-screen");
-const startButton = document.getElementById("start-button");
-
-startButton.addEventListener("click", () => {
-  startScreen.style.display = "none";
-});
-
-window.addEventListener("resize", () => {
-  const newWidth = window.innerWidth;
-  const newHeight = window.innerHeight;
-
-  camera.aspect = newWidth / newHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(newWidth, newHeight);
-});
+camera.position.z = 10;
+camera.position.y = 7;
 
 const clock = new THREE.Clock();
-const scoreElement = document.getElementById("score");
-let score = 0;
-
-function updateScoreDisplay() {
-  scoreElement.textContent = `Score: ${score}`;
-}
-
-function checkCollisions() {
-  const characterPosition = character.position.clone();
-
-  scene.children.forEach((child) => {
-    if (child.userData.type === "brain") {
-      const brainPosition = child.position.clone();
-      const distance = characterPosition.distanceTo(brainPosition);
-
-      if (distance < 1) {
-        score++;
-        updateScoreDisplay();
-
-        scene.remove(child);
-      }
-    }
-  });
-}
 
 function animate() {
-  const deltaTime = clock.getDelta();
+  if (mixer) {
+    mixer.update(clock.getDelta());
+  }
 
-  updateCharacterMovement();
-  updateAnimations(deltaTime);
-  checkCollisions();
+  const animationId = requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
-
-  requestAnimationFrame(animate);
 }
 
 animate();
